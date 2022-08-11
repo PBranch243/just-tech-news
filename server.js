1cd1014436c1231453a0cbb78e5c26c35992e622
Phil Branch <philbranch243@gmail.com>
2022-08-11 11:29:03 -0500

    reset to code snapshot to move on

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-//path used with middleware on comment 13 so we can serve pages
 const path = require('path');
-//handlebar setup
+const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+ 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//comment 13: following middleware allows express to serve static pages
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
-
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
