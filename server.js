e550cb9fbe44ee53a3c9662613b446b5ff6db8c0
Phil Branch <philbranch243@gmail.com>
2022-08-11 10:00:25 -0500

    setup handlebars

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-//
+//path used with middleware on comment 13 so we can serve pages
 const path = require('path');
+//handlebar setup
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+//comment 13: following middleware allows express to serve static pages
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
-//
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
