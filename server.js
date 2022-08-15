66f61911568973211df415329e77b162c884a0f7
Phil Branch <philbranch243@gmail.com>
2022-08-15 10:41:21 -0500

    added logout

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const sess = {
+  //replace secret with actual secret stored in .env
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true, 
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
-const sequelize = require('./config/connection');
+
+
 
 const hbs = exphbs.create({});
  
@@ -17,6 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
